cd1c4b95023c7a70a84e9b3ca11656ba9cd99e1f
Charlotte Strickland <charlottemarie81@gmail.com>
2022-07-30 14:12:36 -0400

    Added new things to breads_controller.js

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,27 +1,23 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
-
-// somewhere at the top with the other dependencies
 const Baker = require('../models/baker.js')
 
 // INDEX
-breads.get('/', (req, res) => {
-  Bread.find()
-    .then(foundBread => {
-      res.render('index', 
-      {
-        breads: foundBread,
-        title: 'Index Page'
-      })
-    })
+breads.get('/', async (req, res) => {
+  const foundBaker = await Baker.find()
+  const foundBread = await Bread.find().limit(2)
+  res.render('index', {
+  title: 'Index Page'
+  })
 })
+
 
 // NEW
 breads.get('/new', (req, res) => {
   Baker.find()
   .then(foundBakers => {
-    res.render('new' {
+    res.render('new', {
       bakers: foundBakers
     })
   })
@@ -41,6 +37,7 @@ breads.get('/:arrayIndex', (req, res) => {
 
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
+  .populate('baker')
     .then (foundBread => {
       const bakedBy = foundBread.getBakedBy()
       console.log(bakedBy)
@@ -104,6 +101,21 @@ breads.get('/:indexArray/edit', (req, res) => {
     index: req.params.indexArray
   })
 })
+
+// EDIT
+breads.get('/:id/edit', (req, res) => {
+  Baker.find()
+    .then(foundBakers => {
+        Bread.findById(req.params.id)
+          .then(foundBread => {
+            res.render('edit', {
+                bread: foundBread, 
+                bakers: foundBakers 
+            })
+          })
+    })
+})
+
 
 
 // UPDATE
--- models/baker.js
+++ models/baker.js
@@ -1,21 +1,28 @@
 // dependencies
+const Bread = require('./bread')
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+
+// Virtuals:
+bakerSchema.virtual('breads', {
+    ref: 'Bread',
+    localField: '_id',
+    foreignField: 'baker'
+})
 
 // schema
 const bakerSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
         enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
-    },
+    }, 
     startDate: {
         type: Date,
         required: true
     },
     bio: String
-})
-
+}, { toJSON: { virtuals: true}})
 
 // model and export
 const Baker = mongoose.model('Baker', bakerSchema)
